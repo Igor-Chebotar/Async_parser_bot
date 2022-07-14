c1015e82cca241309fdb3c0eb22bba3a7a0e25af
Igor_Chebotar <igorcebotar5@gmail.com>
2022-07-14 22:25:32 +0300

    parse_file update

--- pars_file.py
+++ pars_file.py
@@ -61,10 +61,3 @@ async def download(url):
 
 
 url = ['https://ru.wikipedia.org/wiki/%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D1%81%D0%BA%D0%B8%D0%B5_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D1%91%D0%BD%D0%BD%D1%8B%D0%B5_%D0%BF%D1%83%D0%BD%D0%BA%D1%82%D1%8B_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B9_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8']
-
-
-
-# loop = asyncio.get_event_loop()
-# tasks = [asyncio.ensure_future(download(url[0]))]
-# tasks = asyncio.gather(*tasks)
-# loop.run_until_complete(tasks)
