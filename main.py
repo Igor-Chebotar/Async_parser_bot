b5622c24f56d81724b306f04f14f26d78417a395
Igor_Chebotar <igorcebotar5@gmail.com>
2022-07-14 15:03:14 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from secret_token import TOKEN_ST
+from pars_file import download, create_connection, DATABASE_PATH, url
+
+bot = Bot(token=TOKEN_ST)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(msg: types.Message):
+    await msg.answer('Привет')
+
+
+@dp.message_handler(commands=['start'])
+async def help(msg: types.Message):
+    await msg.answer('Для старта введите /start\n'
+                     'Для обновления информации /parse\n'
+                     'Для получения информации о городе введите его название')
+
+
+@dp.message_handler(commands=['parse'])
+async def parsing(msg: types.Message):
+    await download(url[0])
+    await msg.answer('Информация успешно загружена в базу данных')
+
+
+@dp.message_handler(content_types=['text'])
+async def text(msg: types.Message):
+    text_received = msg.text
+    connection = create_connection(DATABASE_PATH)
+    cursor = connection.cursor()
+    res = cursor.execute("SELECT * FROM City WHERE name=?", (text_received,)).fetchone()
+    if res:
+        await msg.answer(f"Город: {res[1]}\n"
+                         f"Население: {res[2]}\n"
+                         f"{res[3]}"
+                         )
+    else:
+        res1 = cursor.execute("SELECT * FROM City WHERE name LIKE ?", ('%' + text_received + '%',)).fetchall()
+        res2 = cursor.execute("SELECT * FROM City WHERE name LIKE ?",
+                              ('%' + text_received.capitalize() + '%',)).fetchall()
+        is_printed = []
+        answer = 'Возможно Вы имели ввиду один из этих городов:\n'
+        for el in res1 + res2:
+            if el[1] not in is_printed:
+                is_printed.append(el[1])
+        answer += '\n'.join(is_printed)
+        await msg.answer(answer)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ pars_file.py
@@ -0,0 +1,69 @@
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+import sqlite3
+from sqlite3 import Error
+
+DATABASE_PATH = 'bot_base.db'
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        raise e
+
+    return connection
+
+
+# Получить веб-страницу (текстовая информация)
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+
+async def parser(html):
+    data = []
+    soup = BeautifulSoup(html, "lxml")
+    book_list = soup.find('table', class_="standard sortable")('td')
+
+    for i in range(0, len(book_list), 8):
+        name = book_list[i + 1].string
+        population = book_list[i + 4].get('data-sort-value')
+        link = 'https://ru.wikipedia.org' + book_list[i + 1].a.get('href')
+        data.append((name, population, link))
+
+    connection = create_connection(DATABASE_PATH)
+    cursor = connection.cursor()
+    query = "INSERT INTO City (name, population, link) VALUES (?, ?, ?)"
+    exist_check = "SELECT * FROM City WHERE name=?"
+    update = "UPDATE City SET population=?, link=? WHERE name=?"
+
+    for row in data:
+        check_update = cursor.execute(exist_check, (row[0],)).fetchone()
+        if check_update:
+            if not (check_update[2] == row[1] and check_update[3] == row[2]):
+                cursor.execute(update, (row[1], row[2], row[0]))
+        else:
+            cursor.execute(query, row)
+    connection.commit()
+
+
+# Обработка веб-страницы
+async def download(url):
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, url)
+        await parser(html)
+        print('all data is parse')
+        return True
+
+
+url = ['https://ru.wikipedia.org/wiki/%D0%93%D0%BE%D1%80%D0%BE%D0%B4%D1%81%D0%BA%D0%B8%D0%B5_%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D1%91%D0%BD%D0%BD%D1%8B%D0%B5_%D0%BF%D1%83%D0%BD%D0%BA%D1%82%D1%8B_%D0%9C%D0%BE%D1%81%D0%BA%D0%BE%D0%B2%D1%81%D0%BA%D0%BE%D0%B9_%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8']
+
+
+
+# loop = asyncio.get_event_loop()
+# tasks = [asyncio.ensure_future(download(url[0]))]
+# tasks = asyncio.gather(*tasks)
+# loop.run_until_complete(tasks)
